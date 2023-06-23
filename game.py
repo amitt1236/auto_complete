11009ec9569030430a2392eed9010ad7608375bb
Hana Hasan <100927079+HanaHasan04@users.noreply.github.com>
2023-06-23 05:54:59 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,66 @@
 import pygame
-import random
+import openai
 import sys
 
-# 0 == NONE, 1 == RIGHT, 2 == LEFT, 3 ==  ENTER
+openai.api_key = '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'
+
+
+def chat_with_chatgpt(user_calmness, keyword, model="gpt-3.5-turbo"):
+    if user_calmness <= 3:
+        calmness_description = "very agitated"
+    elif 4 <= user_calmness <= 7:
+        calmness_description = "moderately calm"
+    else:
+        calmness_description = "very calm"
+
+    prompt = f"""
+    You are an AI assistant helping in a unique scenario. Your task is to suggest commands for another voice assistant named Alessca, who is interacting with a paralyzed patient. The patient communicates with Alessca using an EEG-based thought recognition system. This system provides a single keyword which reflects the patient's thoughts. In addition, you have information about the patient's current state of calmness, which is described as '{calmness_description}'.
+
+    For example, if the patient's calmness level is very low and the keyword is 'son', the commands might be like "Call the patient's son and inform him about the situation" or "Instruct the son to call 101". This indicates that the commands should be related to the keyword and sensitive to the patient's calmness level. 
+
+    Similarly, if the calmness level is very low and the keyword is 'food', the commands might be like "Order food delivery for the patient" or "Call a nurse to bring food", as the patient is likely in need of food.
+
+    On the other hand, if the calmness level is very high, for instance 9, and the keyword is 'son', the commands could be more casual and relaxed, like "Text my son to invite him for a walk in the sun."
+
+    The current calmness level of the patient is: {user_calmness}
+    The keyword from the patient's thoughts is: '{keyword}'
+
+    Based on this information, you need to generate exactly four potential commands for Alessca. These commands should be short, concise, and in the format of an instruction that Alessca can act upon.
+
+    Output your response as a Python list of exactly four short command strings. Include no other text or output. Here is the specific format: ['command 1', 'command 2', 'command 3', 'command 4'].
+    """
+
+    response = openai.ChatCompletion.create(
+        model = model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens = 100
+    )
+
+    message = response.choices[0].message['content']
+
+    # Removing square brackets and quotes, then splitting by comma
+    commands = message.replace('[', '').replace(']', '').replace("'", "").split(', ')
+
+    return commands
+
 
 # TODO: get actual stress level (AMIT&ARIEL!). In the meantime it is number, can be turned into a bool.
 def get_stress_level():
     return 10
 
+
 class renderer():
     def __init__(self):
         pygame.init()
+        self.is_first_call = True
         self.screen_width = 800
         self.screen_height = 600
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("Menu Example")
-        self.font = pygame.font.Font(None, 36)
+        self.font = pygame.font.Font(None, 20)
 
         self.BLACK = (0, 0, 0)
         self.RED = (255, 0, 0)
@@ -25,11 +70,10 @@ class renderer():
         self.WHITE = (255, 255, 255)
 
         stress_threshold = 50
-        stress_level = get_stress_level()
-        self.is_stressed = stress_level > stress_threshold
+        self.stress_level = get_stress_level()
+        self.is_stressed = self.stress_level > stress_threshold
 
-        # TODO: change option names of the menus
-        self.option_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
+        self.option_names = ["Son", "Food", "Bathroom", "Emergency"]
         options = [
             [(0, 0, self.screen_width // 2, self.screen_height // 2), self.option_names[0], self.RED],
             [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), self.option_names[1], self.GREEN],
@@ -52,75 +96,55 @@ class renderer():
         else:
             self.is_ENTER = 1 if gaze_input == 3 else 0
 
-        if self.is_ENTER:
+        if self.is_ENTER and self.is_first_call:
             if self.option_names[self.current_pos] == self.option_names[0]:
-                if self.is_stressed:
-                    is_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[3], self.YELLOW]
-                    ]
-                else:
-                    no_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[3], self.YELLOW]
-                    ]
+                new_options = chat_with_chatgpt(self.stress_level, self.option_names[self.current_pos])
+                self.current_menu = [
+                    [(0, 0, self.screen_width // 2, self.screen_height // 2), new_options[0], self.RED],
+                    [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), new_options[1],
+                     self.GREEN],
+                    [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[2], self.BLUE],
+                    [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[3], self.YELLOW]
+                ]
+                self.is_first_call = False
             elif self.option_names[self.current_pos] == self.option_names[1]:
-                if self.is_stressed:
-                    is_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[3], self.YELLOW]
-                    ]
-                else:
-                    no_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[3], self.YELLOW]
-                    ]
+                new_options = chat_with_chatgpt(self.stress_level, self.option_names[self.current_pos])
+                self.current_menu = [
+                    [(0, 0, self.screen_width // 2, self.screen_height // 2), new_options[0], self.RED],
+                    [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), new_options[1],
+                     self.GREEN],
+                    [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[2], self.BLUE],
+                    [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[3], self.YELLOW]
+                ]
+                self.is_first_call = False
             elif self.option_names[self.current_pos] == self.option_names[2]:
-                if self.is_stressed:
-                    is_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[3], self.YELLOW]
-                    ]
-                else:
-                    no_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[3], self.YELLOW]
-                    ]
+                new_options = chat_with_chatgpt(self.stress_level, self.option_names[self.current_pos])
+                self.current_menu = [
+                    [(0, 0, self.screen_width // 2, self.screen_height // 2), new_options[0], self.RED],
+                    [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), new_options[1],
+                     self.GREEN],
+                    [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[2], self.BLUE],
+                    [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[3], self.YELLOW]
+                ]
+                self.is_first_call = False
             elif self.option_names[self.current_pos] == self.option_names[3]:
-                if self.is_stressed:
-                    is_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), is_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), is_stress_names[3], self.YELLOW]
-                    ]
-                else:
-                    no_stress_names = ["Option 0", "Option 1", "Option 2", "Option 3"]
-                    self.current_menu = [
-                        [(0, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[0], self.RED],
-                        [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), no_stress_names[1], self.GREEN],
-                        [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[2], self.BLUE],
-                        [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2), no_stress_names[3], self.YELLOW]
-                    ]
+                new_options = chat_with_chatgpt(self.stress_level, self.option_names[self.current_pos])
+                self.current_menu = [
+                    [(0, 0, self.screen_width // 2, self.screen_height // 2), new_options[0], self.RED],
+                    [(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height // 2), new_options[1],
+                     self.GREEN],
+                    [(0, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[2], self.BLUE],
+                    [(self.screen_width // 2, self.screen_height // 2, self.screen_width // 2, self.screen_height // 2),
+                     new_options[3], self.YELLOW]
+                ]
+                self.is_first_call = False
 
         self.screen.fill(self.BLACK)
 
